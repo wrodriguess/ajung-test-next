e396b8fcb18bb17ef207cf8f8ce61a58d38dec9e
William <willrodriguess77@gmail.com>
2022-07-01 14:19:33 -0300

    configurações do projeto

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['grodbzf5x83i.compat.objectstorage.sa-saopaulo-1.oraclecloud.com'],
+  },
+}
+
+module.exports = nextConfig
